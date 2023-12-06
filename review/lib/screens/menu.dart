7af07e4e55dc4f063c6ec5848d321ca64a8dcc75
nabilazainina <nabilazainina@gmail.com>
2023-12-06 15:06:02 +0700

    Perbaikan menu.dart

--- review/lib/screens/menu.dart
+++ review/lib/screens/menu.dart
@@ -1,75 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:review/models/models.dart';
-import 'review_form.dart'; // Import your ReviewForm
-import 'package:review/models/models.dart'; // Import your BookReview model
+import 'review_form.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-
-class ReviewHomePage extends StatelessWidget {
+class ReviewHomePage extends StatefulWidget {
   ReviewHomePage({Key? key}) : super(key: key);
+
+  @override
+  _ReviewHomePageState createState() => _ReviewHomePageState();
+}
+
+class _ReviewHomePageState extends State<ReviewHomePage> {
+  Future<List<BookReview>> fetchReviews() async {
+    var url = Uri.parse('https://takugo-c04-tk.pbp.cs.ui.ac.id/bookreview/review-json/<int:book_id>/');
+    var response = await http.get(url, headers: {"Content-Type": "application/json"});
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(utf8.decode(response.bodyBytes)) as List;
+      return data.map<BookReview>((json) => BookReview.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load reviews');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Review'),
+        title: const Text('Home Review'),
       ),
-      body: Column(
-        children: <Widget>[
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const ReviewForm()),
-              );
-            },
-            child: Text('Add Review'),
-          ),
-          Expanded(
-            child: FutureBuilder<List<BookReview>>(
-              future: fetchReviews(), // Implement this function to get reviews
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return CircularProgressIndicator();
-                } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else {
-                  // Display the reviews
-                  return ListView.builder(
-                    itemCount: snapshot.data?.length ?? 0,
-                    itemBuilder: (context, index) {
-                      var review = snapshot.data![index];
-                      return ListTile(
-                        title: Text(review.fields.comment),
-                        subtitle: Text('Rating: ${review.fields.rating}'),
-                      );
-                    },
-                  );
-                }
+      body: FutureBuilder<List<BookReview>>(
+        future: fetchReviews(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text("No reviews available."));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                var review = snapshot.data![index];
+                return ListTile(
+                  title: Text(review.fields.comment),
+                  subtitle: Text('Rating: ${review.fields.rating}'),
+                );
               },
-            ),
-          ),
-        ],
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const ReviewForm()),
+          );
+        },
+        child: const Icon(Icons.add),
       ),
     );
-  }
-
-  // Dummy function to simulate fetching reviews
-  // Replace this with your actual data fetching logic
-  Future<List<BookReview>> fetchReviews() async {
-    // Example of how to use the model
-    return [
-      BookReview(
-        model: 'book.review',
-        pk: 1,
-        fields: Fields(
-          comment: 'Great book!',
-          rating: 5,
-          user: 1,
-          book: 1,
-          dateAdded: DateTime.now(),
-        ),
-      ),
-      // Add more reviews here
-    ];
   }
 }
