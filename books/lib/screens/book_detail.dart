740cee6dacaeeedd6c7819bcf841891c23c47f8c
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2023-12-09 22:47:15 +0900

    edit book detail page UI

--- books/lib/screens/book_detail.dart
+++ books/lib/screens/book_detail.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:books/models/book.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
 
 class ViewBookDetail extends StatelessWidget {
   final Book book;
   const ViewBookDetail(this.book, {super.key});
 
+  Future<String> buyBook(CookieRequest request, int amount) async {
+    final resp = await request.post(
+        'https://takugo-c04-tk.pbp.cs.ui.ac.id/books/buy-book-flutter/${book.pk}/',
+        {'amount': amount});
+
+    if (resp['status']) {
+      return resp['message'];
+    } else {
+      return "Failed to buy book: ${resp['message']}";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+
     return Scaffold(
         appBar: AppBar(
             title: Text(book.fields.title),
-            backgroundColor: Colors.grey[400],
+            backgroundColor: Colors.yellow[700],
             foregroundColor: Colors.black87),
         body: Container(
           padding: const EdgeInsets.all(8),
@@ -21,19 +37,66 @@ class ViewBookDetail extends StatelessWidget {
                 const SizedBox(height: 20),
                 Image.network(book.fields.imageUrl),
                 const SizedBox(height: 10),
-                Text(book.fields.title),
+                Text(book.fields.title,
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold)),
                 const SizedBox(height: 10),
-                Text('Score: ${book.fields.score}'),
+                Text('Score: ${book.fields.score}',
+                    style: const TextStyle(fontSize: 16)),
                 const SizedBox(height: 10),
-                Text('Volumes: ${book.fields.volumes}'),
+                Text('Volumes: ${book.fields.volumes}',
+                    style: const TextStyle(fontSize: 16)),
                 const SizedBox(height: 10),
-                ElevatedButton(onPressed: () {}, child: const Text('Buy Book')),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16, left: 8, right: 8),
+                  child: SizedBox(
+                      height: 40,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          // TODO: Add popup to buy book
+                          String msg = await buyBook(request, 0);
+                          ScaffoldMessenger.of(context)
+                            ..hideCurrentSnackBar()
+                            ..showSnackBar(SnackBar(content: Text(msg)));
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.yellow[700],
+                            foregroundColor: Colors.black87),
+                        child: const Text('Buy Book',
+                            style: TextStyle(fontSize: 16)),
+                      )),
+                ),
                 const SizedBox(height: 10),
-                ElevatedButton(
-                    onPressed: () {}, child: const Text('Review Book')),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8, left: 8, right: 8),
+                  child: SizedBox(
+                      height: 40,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.yellow[700],
+                            foregroundColor: Colors.black87),
+                        child: const Text('Review Book',
+                            style: TextStyle(fontSize: 16)),
+                      )),
+                ),
                 const SizedBox(height: 10),
-                ElevatedButton(
-                    onPressed: () {}, child: const Text('View Journal')),
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: SizedBox(
+                      height: 40,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.yellow[700],
+                            foregroundColor: Colors.black87),
+                        child: const Text('Book Journal',
+                            style: TextStyle(fontSize: 16)),
+                      )),
+                ),
               ],
             ),
           ),
